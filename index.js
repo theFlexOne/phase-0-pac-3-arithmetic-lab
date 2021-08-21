8c3d053d1a0a8d1fda654bd6764a78afc3bcaeab
Eric <edhavey@homemail.com>
2021-08-21 14:18:26 -0500

    Assignment complete

---
+++ index.js
@@ -0,0 +1,34 @@
+// All of these funtions could be written on one line.
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function increment(a) {
+  return ++a;
+}
+
+function decrement(a) {
+  return --a;
+}
+
+function makeInt(n) {
+  return Number.parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return Number.parseFloat(n);
+}
+
